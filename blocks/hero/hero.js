f9ff798e04a2e0e18d3a1c630b91bf8202301c4c
Aaron Stewart <aaron.stewart@cambriausa.com>
2024-06-18 14:11:15 -0700

    Adding desktop and mobile images to hero.

---
+++ blocks/hero/hero.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+    console.log(block);
+    console.log([...block.children]);
+    const hpHero = block.closest('.hero.block');
+  
+    if (hpHero) {
+      const foreground = document.createElement('div');
+      foreground.className = 'hero-foreground';
+  
+      [...block.children].forEach((item, i) => {
+        const childDiv = item.querySelector('div');
+        if (i !== 1) {
+          item.classList.add(`hero-foreground-item-${i}`);
+          foreground.append(item);
+        } else {
+          item.className = 'hero-background';
+        }
+  
+        if (childDiv.innerHTML === '') {
+          childDiv.parentElement.remove();
+        }
+      });
+  
+      block.append(foreground);
+    }
+}
