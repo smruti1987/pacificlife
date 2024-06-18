5fe6650b7f1a157c8391cd1291935037ae61da46
Aaron Stewart <aaron.stewart@cambriausa.com>
2024-06-18 16:46:23 -0700

    Added hero images and added hero js and styles.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,20 +1,24 @@
 export default function decorate(block) {
   const hpHero = block.closest('.hero.block');
   if (hpHero) {
-    const foreground = document.createElement('div');
-    foreground.className = 'hero-foreground';
+    const background = document.createElement('div');
+    background.className = 'hero-background';
     [...block.children].forEach((item, i) => {
       const childDiv = item.querySelector('div');
-      if (i !== 1) {
-        item.classList.add(`hero-foreground-item-${i}`);
-        foreground.append(item);
+      if (i === 2) {
+        item.className = 'hero-foreground';
       } else {
-        item.className = 'hero-background';
+        if (i === 0) {
+          item.classList.add('hero-img-desktop');
+        } else {
+          item.classList.add('hero-img-mobile');
+        }
+        background.append(item);
       }
       if (childDiv.innerHTML === '') {
         childDiv.parentElement.remove();
       }
     });
-    block.append(foreground);
+    block.append(background);
   }
 }
