e19d2d8e1a1b4dca93ed9904b3b9b075e0c29088
Aaron Stewart <aaron.stewart@cambriausa.com>
2024-06-18 14:14:34 -0700

    Fixed linter issues.

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,24 +1,20 @@
 export default function decorate(block) {
-    const hpHero = block.closest('.hero.block');
-  
-    if (hpHero) {
-      const foreground = document.createElement('div');
-      foreground.className = 'hero-foreground';
-  
-      [...block.children].forEach((item, i) => {
-        const childDiv = item.querySelector('div');
-        if (i !== 1) {
-          item.classList.add(`hero-foreground-item-${i}`);
-          foreground.append(item);
-        } else {
-          item.className = 'hero-background';
-        }
-  
-        if (childDiv.innerHTML === '') {
-          childDiv.parentElement.remove();
-        }
-      });
-  
-      block.append(foreground);
-    }
+  const hpHero = block.closest('.hero.block');
+  if (hpHero) {
+    const foreground = document.createElement('div');
+    foreground.className = 'hero-foreground';
+    [...block.children].forEach((item, i) => {
+      const childDiv = item.querySelector('div');
+      if (i !== 1) {
+        item.classList.add(`hero-foreground-item-${i}`);
+        foreground.append(item);
+      } else {
+        item.className = 'hero-background';
+      }
+      if (childDiv.innerHTML === '') {
+        childDiv.parentElement.remove();
+      }
+    });
+    block.append(foreground);
+  }
 }
